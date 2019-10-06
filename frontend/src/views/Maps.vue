4d84d9c238ca8e320e53f0cf8a1f61bfd309f705
nekobato <nekobato@gmail.com>
2019-10-07 02:23:00 +0900

    selected location

--- frontend/src/views/Maps.vue
+++ frontend/src/views/Maps.vue
@@ -3,18 +3,60 @@
     <div class="map-area">
       <img src="/img/18.png" />
     </div>
-    <PulldownButton class="map-name">国会議事堂 衆議院</PulldownButton>
+    <div class="locations-container">
+      <Location
+        class="location"
+        v-for="(location, index) in locations"
+        :key="index"
+        :style="{ left: location.x + 'px', top: location.y + 'px' }"
+        @click.native.stop="onClickLocation(index)"
+      />
+    </div>
+    <PulldownButton class="map-name">{{ map.title }}</PulldownButton>
     <SearchInput class="search-input" placeholder="Search" />
+    <div class="user-info-container" v-show="userInfo.isVisible">
+      <div class="user-info">
+        <span>{{ userInfo.data.name }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import PulldownButton from '@/components/Atoms/PulldownButton.vue';
+import Location from '@/components/Atoms/Location.vue';
 import SearchInput from '@/components/Molecules/SearchInput.vue';
 
 export default Vue.extend({
-  components: { PulldownButton, SearchInput },
+  components: { PulldownButton, SearchInput, Location },
+  data() {
+    return {
+      userInfo: {
+        isVisible: false,
+        locationIndex: 0,
+        data: {
+          name: '',
+        },
+      },
+    };
+  },
+  computed: {
+    map() {
+      return this.$store.state.maps[0];
+    },
+    locations() {
+      return this.$store.state.locations;
+    },
+    users() {
+      return this.$store.state.users;
+    },
+  },
+  methods: {
+    onClickLocation(index: number) {
+      this.userInfo.isVisible = true;
+    },
+  },
 });
 </script>
 
@@ -26,10 +68,34 @@ export default Vue.extend({
   position: absolute;
   top: 16px;
   left: 24px;
+  width: 320px;
 }
 .search-input {
   position: absolute;
   top: 64px;
   left: 24px;
+}
+.locations {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.location {
+  position: absolute;
+}
+.user-info-container {
+  position: fixed;
+  right: 16px;
+  bottom: 16px;
+  width: 320px;
+  height: 240px;
+  .user-info {
+    background: #fff;
+    height: 100%;
+    border: 1px solid rgba(0, 0, 0, 0.48);
+    border-radius: 4px;
+  }
 }
 </style>
